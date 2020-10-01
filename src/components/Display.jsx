a2f3782963b7cf3c39d1d0cac33b3a8c4b9693ba
CiTRuS93 <hadarradah@gmail.com>
2020-10-01 10:56:22 +0300

    search bar

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,15 +1,27 @@
 import React from "react";
 import resources from "../ml_resources.json";
+import { useState } from 'react';
+
 
 const Display = () => {
+  const [search, setSearch] = useState("");
   return (
     <div>
       <h1>
         <span>ML Resources</span>
       </h1>
-
+      <div className="search">
+      <input
+            type="text"
+            placeholder="search"
+            value={search}
+            onChange={(event,value) => setSearch(event.target.value)}
+         />
+      </div>
       <dl className="json">
-        {resources.map((resource, index) => {
+        {resources.filter(
+          (resource => resource.link.includes(search)||resource.description.includes(search)||resource.title.includes(search))
+          ).map((resource, index) => {
           return (
             <div className="bxstyle" key={resource.id}>
               <dt>
