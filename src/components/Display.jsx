bffb02162da38b573ff152e954e21a235efde119
Lubomir <lubomir.yudenko@gmail.com>
2020-10-01 18:06:34 +0300

    Add shuffling to the resources

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,8 +1,22 @@
 import React from "react";
-import resources from "../ml_resources.json";
+import unshuffledResources from "../ml_resources.json";
 import { useState } from "react";
 import MouseTooltip from "react-sticky-mouse-tooltip";
 import { ReactTinyLink } from "react-tiny-link";
+
+function shuffle(ogArr) {
+  // Do cloning to prevent mutation
+  const arr = [...ogArr];
+  for (let i = arr.length - 1; i > 0; i--) {
+    const newI = Math.floor(Math.random() * (i + 1));
+    const tempValue = arr[newI];
+    arr[newI] = arr[i];
+    arr[i] = tempValue;
+  }
+  return arr;
+}
+
+const resources = shuffle(unshuffledResources);
 
 const Display = () => {
   const [search, setSearch] = useState("");
